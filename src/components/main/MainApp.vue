806d814a0768e8b093b04d3708fa62dc1dd7e2ba
LeoSallu <L.sallustio99@gmail.com>
2023-06-20 19:45:20 +0200

    bugggggg

--- src/components/main/MainApp.vue
+++ src/components/main/MainApp.vue
@@ -1,13 +1,30 @@
 <script>
-export default{
-    name:'Main'
-}
+import { useApiStore } from "../../apiStore";
+export default {
+  name: "Main",
+  useApiStore,
+  setup() {
+    const store = useApiStore();
+    const data = store.data;
+    const getData = store.getData;
+    return {
+      data,
+      getData,
+    };
+  },
+  created() {
+    this.getData();
+    
+  },
+};
 </script>
 <template>
-    <main>
-        Main
-    </main>
+  <main>
+    <div class="prova">
+        <ul>
+            <li v-for="apartment in data">{{ apartment.title }}</li>
+        </ul>
+    </div>
+  </main>
 </template>
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import './assets/scss/main.scss';
-
+import { createPinia } from 'pinia';
 import * as bootstrap from 'bootstrap';
 
 import App from './App.vue'
-
+const app = createApp(App)
+const pinia = createPinia();
+app.use(pinia);
 createApp(App).mount('#app')
