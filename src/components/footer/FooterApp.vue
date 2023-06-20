0597ec7b37b458d8dabec9715529f1c67c4ac0cc
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-06-20 19:46:19 +0200

    Footer da finire

--- src/components/footer/FooterApp.vue
+++ src/components/footer/FooterApp.vue
@@ -6,7 +6,21 @@ export default {
 </script>
 <template>
     <footer>
-        Footer
+        <div class="container h-100">
+            <div class="row row-cols-3 text-center h-100 align-items-center">
+                <div>Prova 1</div>
+                <div>Prova 2</div>
+                <div>Prova 3</div>
+            </div>
+        </div>
     </footer>
 </template>
-<style></style>
+<style lang="scss" scoped>
+@use '../../assets/scss/_partial/variables' as *;
+footer{
+    background-color: $primary;
+    width: 100%;
+    height: 3.125rem;
+    color: white;
+}
+</style>
