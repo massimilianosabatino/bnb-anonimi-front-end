250416dc15b0c40f372fac20e7f76f5e3b298259
albertogioia93 <albertogioia@hotmail.it>
2023-06-26 16:32:23 +0200

    scheletro cards aboutus page + favicon

--- src/main.js
+++ src/main.js
@@ -20,6 +20,8 @@ import { faFan } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
@@ -27,7 +29,7 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 /* add icons to the library */
 
-library.add(faUser,faBars,faEnvelope,faInstagram,faTwitter,faFacebook,faFan);
+library.add(faUser,faBars,faEnvelope,faInstagram,faTwitter,faFacebook,faFan,faGithub,faLinkedin);
 
 
 const pinia = createPinia();
--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,20 +1,85 @@
-<script>
-export default {
-    name: 'AboutUs'
-}
-</script>
-
 <template>
     <main>
-        <div class="container  my-2">
-            <h1>About Us</h1>
+        <div class="container m-5">
+            <h1>About Us:</h1>
         </div>
-
+        <div class="container d-flex justify-content-center">
+            <div class="row align-items-stretch carte">
+                <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-1 my-1" v-for="(developer, index) in developers" :key="developer.id">
+                    <div class="card h-100">
+                        <img :src="developer.image" class="card-img-top" alt="Developer Image">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">{{ developer.name }}</h5>
+                            <div class="mt-auto d-flex flex-column">
+                                <a :href="developer.github" class="mb-2 link-no-underline"><font-awesome-icon icon="fa-brands fa-github" /> GitHub</a>
+                                <a :href="developer.linkedin" class="link-no-underline"><font-awesome-icon icon="fa-brands fa-linkedin" /> LinkedIn</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
-
+  
+<script>
+export default {
+    name: 'AboutUs',
+    data() {
+        return {
+            developers: [
+                {
+                    id: 1,
+                    name: 'Alberto Gioia',
+                    image: 'src/assets/img/darth vader.png',
+                    github: 'https://github.com/albertogioia93'
+                },
+                {
+                    id: 2,
+                    name: 'Massimiliano Sabatino',
+                    image: 'src/assets/img/ichigo.png',
+                    github: 'https://github.com/albertogioia93'
+                },
+                {
+                    id: 3,
+                    name: 'Mattia Fezzardi',
+                    image: 'src/assets/img/gon.png',
+                    github: 'https://github.com/albertogioia93'
+                },
+                {
+                    id: 4,
+                    name: 'Leonardo Sallustio',
+                    image: 'src/assets/img/goku.png',
+                    github: 'https://github.com/albertogioia93'
+                },
+                {
+                    id: 5,
+                    name: 'Luca Zanfrisco',
+                    image: 'src/assets/img/rocket.png',
+                    github: 'https://github.com/albertogioia93'
+                }
+            ]
+        };
+    }
+}
+</script>
+  
 <style scoped lang="scss">
-main{
+main {
     height: 100vh;
 }
+.carte {
+    justify-content: space-between;
+}
+
+.link-no-underline {
+  text-decoration: none;
+}
+
 </style>
+  
+  
+  
+  
+  
+  
