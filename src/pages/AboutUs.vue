6a5ce1aa63f1ebf21545af79b584c340039682a4
LeoSallu <L.sallustio99@gmail.com>
2023-06-26 11:32:31 +0200

    feat:About Us pages in router

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AboutUs'
+}
+</script>
+
+<template>
+    <main>
+        <div class="container  my-2">
+            <h1>About Us</h1>
+        </div>
+
+    </main>
+</template>
+
+<style scoped lang="scss">
+main{
+    height: 100vh;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,8 +3,8 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import SearchPage from "./pages/SearchPage.vue";
 import ContactPage from "./pages/ContactPage.vue";
-import DetailsPage from "./pages/DetailsPage.vue"
-
+import DetailsPage from "./pages/DetailsPage.vue";
+import AboutUs from "./pages/AboutUs.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -27,7 +27,13 @@ const router = createRouter({
       path: '/apartment',
       name:'Details',
       component:DetailsPage
+    },
+    {
+      path:'/about',
+      name:'AboutUs',
+      component:AboutUs
     }
+
   ],
 });
 
