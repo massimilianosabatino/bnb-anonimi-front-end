d8c9e377645db41847afa0cf50cabf36adb54fd7
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-06-28 09:41:23 +0200

    Ricerca da HomePage

--- src/components/utils/SearchApp.vue
+++ src/components/utils/SearchApp.vue
@@ -6,26 +6,32 @@ export default {
   name: "SearchApp",
   methods: {
     ...mapActions(useSearchStore, ["searchApartment"]),
-    ...mapActions(useSearchStore,["chePalle"]),
+    ...mapActions(useSearchStore, ["chePalle"]),
   },
-  mounted(){
+  mounted() {
     this.chePalle();
   }
 };
 </script>
 <template>
   <!--Search-->
-      <div id="search">
-        <form @submit.prevent="searchApartment" class="d-flex justify-content-center gap-3" role="search">
-          <div id="address"></div>
-          <button class="btn btn-sm btn-success" type="submit">Cerca</button>
-        </form>
-      </div>
+  <div id="search">
+    <form @submit.prevent="searchApartment" class="d-flex justify-content-center align-items-center gap-3" role="search">
+      <div id="address"></div>
+      <router-link :to="{ name: 'Search' }">
+        <button class="btn btn-outline-success" type="submit" @click="searchApartment()" id="cerca">Cerca</button>
+      </router-link>
+    </form>
+  </div>
   <!--/Search-->
 </template>
 <style lang="scss" scoped>
-  #address{
-    width: 75%;
-    background-color: transparent;
-  }
+@use '../../assets/scss/_partial/variables' as *;
+#address {
+  width: 75%;
+  background-color: transparent;
+}
+.btn-outline-success:hover{
+  background-color: $link;
+}
 </style>
