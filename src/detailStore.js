b66260ca2abd9c64717feb550afe1023a419c8c8
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-06-29 16:16:07 +0200

    Invio messaggio con risposta

--- src/detailStore.js
+++ src/detailStore.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { defineStore } from 'pinia';
 import { router } from "../src/router";
+import { faI } from "@fortawesome/free-solid-svg-icons";
 
 
 export const useDetailStore = defineStore('detail', {
     state: () => {
         return {
             api: 'http://127.0.0.1:8000/api/apartment/',
+            messageApi: 'http://127.0.0.1:8000/api/message',
             detail: null,
+            name: '',
+            email: '',
+            content: '',
+            messageSuccess: null,
+            messageResult: null,
         }
     },
     actions: {
@@ -15,7 +22,7 @@ export const useDetailStore = defineStore('detail', {
             axios.get(`${this.api}${router.currentRoute.value.params.id}`)
                 .then((response) => {
                     this.detail = response.data.results;
-                }).then(() =>{
+                }).then(() => {
                     this.apartmentMap();
                 })
         },
@@ -29,5 +36,33 @@ export const useDetailStore = defineStore('detail', {
             })
             var marker = new tt.Marker().setLngLat(HQ).addTo(map);
         },
+        sendMessage() {
+            const today = new Date().toJSON().slice(0, 10);
+            this.messageSucces = null;
+            this.messageResult = null;
+            axios.post(this.messageApi, {
+                id: this.detail.id,
+                name: this.name,
+                email: this.email,
+                content: this.content,
+                date: today,
+            }).then((response) => {
+                this.messageResult = response.data.result;
+                this.messageSuccess = response.data.success;
+            }).then(() => {
+                let fail = document.getElementById('fallito');
+                let send = document.getElementById('inviato');
+                if (fail) {
+                    setInterval(function () {
+                        fail.classList.add('d-none');
+                    }, 3000)
+                }
+                if(send){
+                    setInterval(function () {
+                        send.classList.add('d-none');
+                    }, 3000)
+                }
+            })
+        }
     }
 })
